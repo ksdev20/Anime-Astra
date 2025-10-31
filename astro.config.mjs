15a0291cc24d65017b36aab56278894960638c12
ksdev20 <batbale01@gmail.com>
2025-10-31 14:41:41 +0530

    removed robots.txt from root 2. Modified sitemap link in robots.txt in /public 3. Implemented Astro sitemap, expecting them at /sitemap-index.xml and /sitemap-0.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import vercelAdapter from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://animeastra.vercel.app/",
+  integrations: [react(), sitemap()],
   adapter: vercelAdapter(),
   output: 'server',
   devToolbar: {
